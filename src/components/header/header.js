ea559d85723397c735bace3ebf0d9c6d4fb9b189
Malik <madkonline@gmail.com>
2021-09-13 09:37:43 +0530

    Cleanup boilerplate code

---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <span className="header-container">
+        this is new header
+      </span>
+    );
+  }
+}
+
+export default Header;
