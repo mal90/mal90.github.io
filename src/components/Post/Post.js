9ec63c8dbb86d6aef32335d673e6afe7509a86f0
Malik <madkonline@gmail.com>
2021-09-19 00:28:27 +0530

    Fix lint issues

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -12,15 +12,13 @@ const Post = (props) => {
   }
 
   const fetchedPost = {}
-  let postExists = false
 
   postlist.forEach((post, i) => {
       if (validId === post.id) {
-          fetchedPost.title = post.title ? post.title : "No title given"
-          fetchedPost.date = post.date ? post.date : "No date given"
-          fetchedPost.author = post.author ? post.author : "No author given"
-          fetchedPost.content = post.content ? post.content : "No content given"
-          postExists = true
+        fetchedPost.title = post.title ? post.title : "No title given"
+        fetchedPost.date = post.date ? post.date : "No date given"
+        fetchedPost.author = post.author ? post.author : "No author given"
+        fetchedPost.content = post.content ? post.content : "No content given"
       }
   })
 
