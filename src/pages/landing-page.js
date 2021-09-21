1ef0982a44376013f271c1e71d5c0264c8c2bfc8
Malik <madkonline@gmail.com>
2021-09-21 20:51:29 +0530

    remove unused

--- src/pages/landing-page.js
+++ src/pages/landing-page.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect }  from "react";
+import React  from "react";
 import {
   Switch,
   Route,
-  useLocation
 } from "react-router-dom";
 import About from "./about";
 import Blog from "./blog";
